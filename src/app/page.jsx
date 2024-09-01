40bc1e18f80002b9ed1a194b102eb03a82df2a74
hirajya <rodneyestrada2425@gmail.com>
2024-09-01 12:24:06 +0800

    .

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,28 @@
+import styles from "./home.module.css";
 import Image from "next/image";
-import styles from "./page.module.css";
 
 const Home = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creatives Thoughts Agency.</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brand} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
+};
 
 export default Home;
