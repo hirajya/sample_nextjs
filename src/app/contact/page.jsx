2e6013eec4e70d55296877d87ae9eff5ee2f964f
hirajya <rodneyestrada2425@gmail.com>
2024-09-03 22:25:02 +0800

    .

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,23 +1,42 @@
-import Image from 'next/image';
-import styles from './contact.module.css';
+"use client";
+
+import Image from "next/image";
+import styles from "./contact.module.css";
+// import HydrationTest from "@/components/hydrationTest";
+import dynamic from "next/dynamic";
+
+// const HydrationTestNoSSR = dynamic(() => import("@/components/hydrationTest"), { ssr: false });
+
 
 const ContactPage = () => {
-return (
+  const a = Math.random();
+  console.log(a);
+  
+  return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
-        <Image src="/contact.png" alt="" fill className={styles.img}/>
+        <Image src="/contact.png" alt="" fill className={styles.img} />
       </div>
       <div className={styles.formContainer}>
+        {/* <HydrationTestNoSSR /> */}
+        <div suppressHydrationWarning>{a}</div>
         <form action="" className={styles.form}>
-          <input type="text" placeholder='Name and Surname'/>
-          <input type="text" placeholder='Email Address'/>
-          <input type="text" placeholder='Phone Number (Optional)'/>
-          <textarea name="" id="" cols="30" rows="10" type="text" placeholder='Message'/>
+          <input type="text" placeholder="Name and Surname" />
+          <input type="text" placeholder="Email Address" />
+          <input type="text" placeholder="Phone Number (Optional)" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            type="text"
+            placeholder="Message"
+          />
           <button>Send</button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ContactPage;
